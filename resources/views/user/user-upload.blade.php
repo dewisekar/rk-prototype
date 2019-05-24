b3cca061f3ded0e29f048f8aac99d8264df6cbd1
isyepr <32873316+isyepr@users.noreply.github.com>
2019-05-24 12:43:36 +0700

    Update user-upload.blade.php

--- resources/views/user/user-upload.blade.php
+++ resources/views/user/user-upload.blade.php
@@ -130,68 +130,126 @@
 							<input type="text" name="" placeholder="Judul" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Judul'"
 							 required class="single-input">
 						</div>
-						<div class="mt-10 text-left ">
-							<input type="email" name="" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'"
-							 required class="single-input">
-                        </div>
-                        <div class="mt-10">
-							<input type="file" name="" placeholder="Kartu Keanggotaan ITS" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Kartu Keanggotaan ITS'"
-							 required class="single-input">
-						</div>
-						<div class="input-group-icon mt-10">
-                            <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div>
-							<div class="form-select" id="default-select">
-								<select>
-									<option value="1">Fakultas</option>
-									<option value="1">FIA</option>
-									<option value="1">FTI</option>
-									<option value="1">FTSLK</option>
-                                    <option value="1">FTK</option>
-                                    <option value="1">FTIK</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-group-icon mt-10">
-							<div class="icon"><i class="ti-map" aria-hidden="true"></i></div>
-							<div class="form-select" id="default-select2">
-								<select>
-									<option value="1">Departemen</option>
-									<option value="1">T. Perkapalan</option>
-									<option value="1">T. Sistem Perkapalan</option>
-									<option value="1">T. Kelautan</option>
-									<option value="1">T. Transportasi Laut</option>
-								</select>
-							</div>
+                        <div class="mt-10 text-left ">
+                            <label class="text-white"> Abstrak </label>
+                            <input type="text" name="" placeholder="Abstrak" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Abstrak'"
+                             required class="single-input">
                         </div>
                         <div class="input-group-icon mt-10">
-							<div class="icon"><i class="fa fa-user" aria-hidden="true"></i></div>
-							<div class="form-select" id="default-select2">
-								<select>
-									<option value="1">Jabatan</option>
-									<option value="1">Dosen</option>
-									<option value="1">Tenaga Didik</option>
-									<option value="1">Mahasiswa</option>
-								</select>
-							</div>
-						</div>
-						<div class="mt-10">
-							<input type="text" name="" placeholder="NRP/NIP" onfocus="this.placeholder = ''" onblur="this.placeholder = 'NRP/NIP'"
-							 required class="single-input-primary">
-						</div>
-						<div class="mt-10">
-							<input type="text" name="first_name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'"
-							 required class="single-input-accent">
-						</div>
-						<div class="mt-10">
-							<input type="password" name="first_name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'"
-							 required class="single-input-secondary">
+                           
+                            <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white"> Program Studi </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">Diploma</option>
+                                    <option value="1">Sarjana</option>
+                                    <option value="1">Pascasarjana</option>
+                                    </select>
+                            </div>
                         </div>
-                        <div class="mt-10">
-							<input type="password" name="first_name" placeholder="Password Confirmation" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password Confirmation'"
-							 required class="single-input-secondary">
+                        <div class="mt-10 text-left ">
+                            <label class="text-white"> Nama Penulis </label>
+                            <input type="text" name="" placeholder="Nama Depan" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Depan'"
+                             required class="single-input">
                         </div>
+                        <div class="mt-10 text-left ">
+                            <input type="text" name="" placeholder="Nama Belakang" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Belakang'"
+                             required class="single-input">
+                        </div>
+						 <div class="mt-10 text-left ">
+                            <label class="text-white"> Dosen Pembimbing 1 </label>
+                            <input type="text" name="" placeholder="Nama Depan" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Depan'"
+                             required class="single-input">
+                        </div>
+                        <div class="mt-10 text-left ">
+                            <input type="text" name="" placeholder="Nama Belakang" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Belakang'"
+                             required class="single-input">
+                        </div>
+                        <div class="mt-10 text-left ">
+                            <label class="text-white"> Dosen Pembimbing 2 </label>
+                            <input type="text" name="" placeholder="Nama Depan" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Depan'"
+                             required class="single-input">
+                        </div>
+                        <div class="mt-10 text-left ">
+                            <input type="text" name="" placeholder="Nama Belakang" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nama Belakang'"
+                             required class="single-input">
+                        </div>
+                        <div class="input-group-icon mt-10">
+                           
+                            <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white"> Bidang </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">Struktur Air</option>
+                                    <option value="1">Teknologi Lepas Pantai</option>
+                                    <option value="1">Pengelasan</option>
+                                    <option value="1">Energi Laut</option>
+                                    <option value="1">Pengelasan</option>
+                                    </select>
+                            </div>
+                        </div>
+                        <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white"> Tahun </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">2019</option>
+                                    <option value="1">2018</option>
+                                    <option value="1">2017</option>
+                                    <option value="1">2016</option>
+                                    <option value="1">2015</option>
+                                    </select>
+                            </div>
+                        </div>
+                          <div class="input-group-icon mt-10">
+                           
+                            <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white"> Bulan </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">Januari</option>
+                                    <option value="1">Feburari</option>
+                                    <option value="1">Maret</option>
+                                    </select>
+                        <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white">Fakultas </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">FS</option>
+                                    <option value="1">FTI</option>
+                                    <option value="1">Maret</option>
+                                    </select>
+                            </div>
+                         </div>
+                       <div class="form-select" id="default-select">
+                                <div class="mt-10 text-left ">
+                                <label class="text-white">Departemen </label>
+                                 <!-- <div class="icon"><i class="fas fa-school" aria-hidden="true"></i></div> -->
+                                <select>
+                                   <!--  <option value="1">Program Studi</option> -->
+                                    <option value="1">T. Perkapalan</option>
+                                    <option value="1">T. Sistem Perkapalan</option>
+                                    <option value="1">T. Kelautan</option>
+                                    </select>
+                            </div>
+                         </div>
+                         <div class="mt-10 text-left ">
+                            <!-- <label class="text-white"> Jumlah Halaman </label> -->
+                            <input type="text" name="" placeholder="Jumlah Halaman" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Jumlah Halaman'"
+                             required class="single-input">
+                        </div>
+						
                         <div class="mt-10">
-                            <button type="submit" class="genric-btn primary circle">Register</button>
+                            <button type="submit" class="genric-btn primary circle">Upload</button>
                         </div>                        
 					</form>
                 </div>
